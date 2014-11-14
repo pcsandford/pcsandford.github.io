8d5db5056a0146a08bb468ed9e9afa8375993609
pcsandford <petersandford1982@gmail.com>
2014-11-14 17:09:36 -0500

    reworked entire js file to include changes to timing and manual functions. Now includes GSAP to control animation events and jquery to control touches and manual scrolling. Touch to be replaced by swipe in next revision. Added new screenshot for new layout including new image for mock-logo, new image fade, new header and header animation.

--- script.js
+++ script.js
@@ -1,114 +1,241 @@
+$(document).ready(function(){
+ console.log('ready');
 
-  $(document).ready(function(){
-    console.log('ready');
-    $('html').on('click',function(){
-    $('.leftCover').css("left","-50vw");
-    $('.rightCover').css("right","-50vw");
-    $('.logoAreaMain').fadeIn().css("opacity","1");
+
+ var rightCover = $('.rightCover'),
+     leftCover = $('.leftCover'),
+     headerText = $('.headerText'),
+     logoAreaMain = $('.logoAreaMain')
+     tl = new TimelineMax();
+
+
+ //setting up the opening timeline for the animation
+ var automaticSliderTimer
+ var automaticCloseTimer
+ var automaticOpenTimer
+ var manualCloseTimer
+ var openTimeline = new TimelineMax();
+ function getOpenAnimation(){
+       openTimeline.to(headerText,3,{opacity:'1'});
+       openTimeline.to(headerText,0,{opacity:'0'});
+       openTimeline.to(headerText,0,{top:'-100%'})
+       openTimeline.to(rightCover,0, {right:'-50vw'});
+       openTimeline.to(leftCover,0, {left:'-50vw'});
+       openTimeline.to(logoAreaMain,0,{opacity:'1'})
+       openTimeline.addLabel('openComplete');
+
+     return openTimeline;
+   }
+
+ //add the items to the opening timeline
+     
+   tl.add('headerText')
+     .add('leftCover')
+     .add('rightCover')
+     .add('logoAreaMain')
+
+ //the function that plays the opening animation
+ function playOpenAnimation(){
+     console.log("open");
+     tl.play(getOpenAnimation());
+     // numberofSlides = 0;
+      stopAutoPlay();
+     automaticSliderTimer = setTimeout(startAutoPlay, 1000 );
+     automaticCloseTimer = setTimeout(playCloseAnimation, 45000 );
+     clearTimeout(automaticOpenTimer);
+    };
+ playOpenAnimation();
+ 
+
+ //auto open on touch
+ $('.leftCover, .rightCover, .headerText').on('click',function(){
+     console.log('jump to open and pause');
+     openTimeline.progress(5, false);
+     manualCloseTimer = setTimeout(playCloseAnimation, 10000 );
+     clearTimeout(automaticCloseTimer);
+     clearTimeout(playOpenAnimation);
+     clearTimeout(automaticSliderTimer);
+   });
+
+ //setting up the closing timeline animaition
+  
+ var tl2 = new TimelineMax();
+
+ var closeTimeline = new TimelineMax();
+
+
+
+ function getCloseAnimation(){
+       closeTimeline.to(rightCover,0, {right:'0'});
+       closeTimeline.to(leftCover,0, {left:'0'});
+       closeTimeline.to(headerText,0,{top:'50%'})
+       closeTimeline.to(logoAreaMain,0,{opacity:'0'})
+       closeTimeline.addLabel('closeComplete');
+       automaticOpenTimer = setTimeout
+
+     return closeTimeline;
+   }
+
+
+ //add the items to the opening timeline
+     
+   tl2.add('leftCover')
+      .add('rightCover')
+      .add('logoAreaMain')
+      .add('headerText')
+
+ 
+      //the function that plays the opening animation
+      function playCloseAnimation(){
+          console.log("close");
+          tl2.play(getCloseAnimation());
+          stopAutoPlay();
+          clearTimeout(automaticCloseTimer);
+          clearTimeout(automaticSliderTimer);
+          automaticOpenTimer = setTimeout(playOpenAnimation, 3000);
+         };
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=======
+//slide controls
+//=======
+var $slider = $(".slider");
+var $slides = $slider.find(".slider-item");
+var $navPrev = $(".previousTrigger");
+var $navNext = $(".nextTrigger");
+
+
+var slidesNum = $slides.length;
+var prevSlideID = null;
+var currentSlideID = 0;
+var isAnimating = false;
+var isAutoPlay = false;
+
+$navPrev.on('click', function(){
+  console.log('go previous')
+  clearTimeout(automaticSliderTimer);
+  clearTimeout(automaticOpenTimer);
+  clearTimeout(manualCloseTimer);
+  clearTimeout(automaticCloseTimer);
+  manualCloseTimer = setTimeout(playCloseAnimation, 10000 );
+});
+
+$navNext.on('click', function(){
+  console.log('go next')
+  clearTimeout(automaticSliderTimer);
+  clearTimeout(automaticOpenTimer);
+  clearTimeout(manualCloseTimer);
+  clearTimeout(automaticCloseTimer);
+  manualCloseTimer = setTimeout(playCloseAnimation, 10000 );
+});
+
+
+function init() {
+  TweenLite.set($slides, {
+    left: "-100%"
   });
-
-//timeout resetting the curtain now set to 2 minutes so effectively 
-//will work 60 seconds after initial opening if no touch detected
-
-var activityTimeout = setTimeout(inActive, 120000);
-
-
-function resetActive(){
-    $(document.body).attr('class', 'browsing');
-    clearTimeout(activityTimeout);
-    clearTimeout(automaticSliderTimer);
-    //timeout in this case set to 60s if touch has been detected
-    activityTimeout = setTimeout(inActive, 60000);
-    // clearInterval(openingTimer);
-    // openingTimer = setInterval(autoOpen,20000);
+  $navPrev.on("click", gotoPrevSlide);
+  $navNext.on("click", gotoNextSlide);
+  gotoSlide(0, 0);
 }
 
-// No activity will reset the screen and bring back the title in transition.
-function inActive(){
-    $(document.body).attr('class', 'inactive');
-    $('.leftCover').css("left","0vw");
-    $('.rightCover').css("right","0vw");
-    $('.logoAreaMain').fadeOut().css("opacity","0");
-    clearInterval(openingTimer);
-    openingTimer = setInterval(autoOpen,60000);
-    clearTimeout(activityTimeout);
-    activityTimeout = setTimeout(inActive, 120000);
-    clearTimeout(automaticSliderTimer);
-    automaticSliderTimer = setInterval(slideSwitch, 10000 );
+function gotoPrevSlide() {
+  var slideToGo = currentSlideID - 1;
+  if (slideToGo <= -1) {
+    slideToGo = slidesNum - 1;
+  }
+  stopAutoPlay();
+  gotoSlide(slideToGo, 1, "prev");
 }
 
-// Check to see if the user has clicked left or right and if so reset the timer.
-$(document).bind('click', function(){resetActive()});
+function gotoNextSlide() {
+  var slideToGo = currentSlideID + 1;
+  if (slideToGo >= slidesNum) {
+    slideToGo = 0;
+  }
+  stopAutoPlay();
+  gotoSlide(slideToGo, 1, "next");
+}
 
+function gotoSlide(slideID, _time, _direction) {
+  if (!isAnimating) {
+    isAnimating = true;
+    prevSlideID = currentSlideID;
+    currentSlideID = slideID;
+    var $prevSlide = $slides.eq(prevSlideID);
+    var $currentSlide = $slides.eq(currentSlideID);
+    var time = 1;
+    if (_time !== null) {
+      time = _time;
+    }
+    var direction = "next";
+    if (_direction != null) {
+      direction = _direction;
+    }
+    if (direction == "next") {
+      TweenLite.to($prevSlide, time, {
+        left: "-100%"
+      });
+      TweenLite.fromTo($currentSlide, time, {
+        left: "100%"
+      }, {
+        left: "0"
+      });
+    } else {
+      TweenLite.to($prevSlide, time, {
+        left: "100%"
+      });
+      TweenLite.fromTo($currentSlide, time, {
+        left: "-100%"
+      }, {
+        left: "0"
+      });
+    }
+    TweenLite.delayedCall(time, function() {
+      isAnimating = false;
+    });
+  }
+}
 
-//slider functionality
+function play(){
+  gotoNextSlide();
+  TweenLite.delayedCall(10, play);
+}
 
-$('.sp').first().addClass('active');
-$('.sp').hide();    
-$('.active').show();
-
-    $('.nextTrigger').click(function(){
-      clearTimeout(automaticSliderTimer);
-      $('.active').removeClass('active').addClass('oldActive');    
-        if ( $('.oldActive').is(':last-child')) {
-          $('.sp').first().addClass('active');
-        }
-        else{
-          $('.oldActive').next().addClass('active');
-        }
-      $('.oldActive').removeClass('oldActive');
-      $('.sp').fadeOut();
-      $('.active').fadeIn();  
-    });//close next trigger function
+function startAutoPlay(immediate) {
+  // clearTimeout(activityTimeout);
+  if (immediate != null) {
+    immediate = false;
+  }
     
-      $('.previousTrigger').click(function(){
-         clearTimeout(automaticSliderTimer);
-        $('.active').removeClass('active').addClass('oldActive');    
-          if ( $('.oldActive').is(':first-child')) {
-        $('.sp').last().addClass('active');
-        }
-          else{
-            $('.oldActive').prev().addClass('active');
-          }
-      $('.oldActive').removeClass('oldActive');
-      $('.sp').fadeOut();
-      $('.active').fadeIn();
-    });//close previous trigger function
+  if (immediate) {
+    gotoNextSlide();
+  }
+  TweenLite.delayedCall(10, play);
+}
 
-      
-    //open the screens
+// var activityTimeout = setTimeout(startAutoPlay, 5000);
 
-      var openingTimer = setInterval(autoOpen,60000);
-      var automaticSliderTimer = setInterval(slideSwitch, 10000 );
+function stopAutoPlay() {
+  // activityTimeout = setTimeout(startAutoPlay, 5000);
+  isAutoPlay = false;
+  TweenLite.killDelayedCallsTo(play);
+}
 
-      function autoOpen(){
-        console.log('autoOpen');
-        $('.leftCover').css("left","-50vw");
-        $('.rightCover').css("right","-50vw");
-        $('.logoAreaMain').fadeIn().css("opacity","1");
-        clearInterval(openingTimer);
-      };
-       
-       function slideSwitch(){
-        $('.active').removeClass('active').addClass('oldActive');    
-        if ( $('.oldActive').is(':last-child')) {
-          $('.sp').first().addClass('active');
-        }
-          else{
-            $('.oldActive').next().addClass('active');
-        }
-      $('.oldActive').removeClass('oldActive');
-      $('.sp').fadeOut();
-      $('.active').fadeIn(); 
-      };
 
-  });//close main function
+init();
 
 
 
-
-
-
-
-
-        
+});//close document ready
